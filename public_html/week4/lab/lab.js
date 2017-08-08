781c24f848688030a6f48bafa7e5c48d3554b4cf
001400799 <001400799@N101A6.labs.neit.edu>
2017-08-07 20:57:20 -0400

    Merge origin/master
    Conflicts:
    	public_html/week1/demo/conditions.html
    	public_html/week1/demo/css-html-review.html
    	public_html/week1/demo/functions.html
    	public_html/week1/demo/loops.html
    	public_html/week1/demo/variables.html

---
+++ public_html/week4/lab/lab.js
@@ -0,0 +1,65 @@
+let input = window.document.querySelector('#num');
+let button = window.document.querySelector('#btnGenerate');
+let div = window.document.querySelector('#wrapper');
+
+
+
+
+function addText() {
+    var result = input.value;
+    div.innerHTML = result;
+    var items = [];
+    var str = "";
+    var ttl = 0;
+    var cntr = 0;
+    var average = 0;
+    result = parseInt(result);
+    
+    str += '<table border="1">';
+    
+    
+    for (let row = 0; row < result; row++) {
+        items[row] = [];
+        str += '<tr>';
+        for (let col = 0; col < result; col++) {
+            items[row][col] = randomNumber(1,100);
+            
+            ttl + average;
+            
+            if ( items[row][col] % 3 === 0){
+            
+                str += '<td class = "red">' + items[row][col] + '</td>';
+            }
+             
+            else if ( items[row][col] % 2 === 0){
+                 str += '<td class = "blue">' + items[row][col] + '</td>';
+             }
+             
+            else {
+                 str += '<td>' + items[row][col] + '</td>';
+             }
+            
+        }
+        str += '</tr>';
+    }
+
+    str += '</table>';
+    div.innerHTML = str;
+    
+
+
+    console.log(items);
+}
+
+button.addEventListener('click', addText);
+
+function randomNumber(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+
+
+
+
