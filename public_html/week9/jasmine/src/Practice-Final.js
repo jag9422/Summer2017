d1505c6ef38943aee98a91fbb60c95ae9334ceca
001400799 <001400799@N101A6.labs.neit.edu>
2017-09-11 18:22:10 -0400

    Merge origin/master
    Conflicts:
    	public_html/week1/demo/conditions.html
    	public_html/week1/demo/css-html-review.html
    	public_html/week1/demo/functions.html
    	public_html/week1/demo/loops.html
    	public_html/week1/demo/variables.html

--- public_html/week9/jasmine/src/Practice-Final.js
+++ public_html/week9/jasmine/src/Practice-Final.js
@@ -6,53 +6,59 @@
  * @return {object} JSON
  */
 function returnJSON() {
+    let person =
+            {
+                name: "John Doe",
+                id: "0123456789"
 
-}
+            };
 
-/**
- * Given the input <input type="text" name="fullname" /> put a value of your choosing.
- * 
- * @return {String} The value
- */
-function insertValueIntoInput(){
-
-}
-
-/**
- * Given the input <input type="text" name="fullname" /> put a class name of your choosing.
- * 
- * @return {String} The value
- */
-function insertClassNameIntoInput(){
-
-}
-
-/**
- * Given the HTML <button>Click ME</button> 
- * When the click the button it should add a paragraph tag to the page
- * 
- * @return {void}
- */
-function buttonEvent(){
-
-}
-
-
-
-/**
- * Check an array matrix of 4x4 to return the largest number found.
- * example of the array
- * [[1, 2, 3, 4], [4, 5, 6, 7], [8, 9, 0, 1], [10, 9, 8, 7]]
- * and 
- * [[10, 20, 30, 40], [100, 50, 60, 70], [80, 90, 0, 10], [4, 90, 80, 70]]
- * 
- * @param {Array} arr 
- * 
- * @return {Number} largest
- */
-function largestNumberOfFourArrays(arr){
-     
+    return person;
     
-}
+    /**
+     * Given the input <input type="text" name="fullname" /> put a value of your choosing.
+     * 
+     * @return {String} The value
+     */
+    function insertValueIntoInput() {
+
+    }
+
+    /**
+     * Given the input <input type="text" name="fullname" /> put a class name of your choosing.
+     * 
+     * @return {String} The value
+     */
+    function insertClassNameIntoInput() {
+
+    }
+
+    /**
+     * Given the HTML <button>Click ME</button> 
+     * When the click the button it should add a paragraph tag to the page
+     * 
+     * @return {void}
+     */
+    function buttonEvent() {
+
+    }
+
+
+
+    /**
+     * Check an array matrix of 4x4 to return the largest number found.
+     * example of the array
+     * [[1, 2, 3, 4], [4, 5, 6, 7], [8, 9, 0, 1], [10, 9, 8, 7]]
+     * and 
+     * [[10, 20, 30, 40], [100, 50, 60, 70], [80, 90, 0, 10], [4, 90, 80, 70]]
+     * 
+     * @param {Array} arr 
+     * 
+     * @return {Number} largest
+     */
+    function largestNumberOfFourArrays(arr) {
+
+
+    }
 
 
